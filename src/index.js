6cba11a26bb4a5db7fc92b73536bc1c39533f75d
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-01-20 11:16:47 +0200

    Remove old examples

--- src/index.js
+++ src/index.js
@@ -1,6 +1,2 @@
 console.log('hello webpack2');
-const common = require('./common.js');
-common.hello();
-import es from './es.js';
-es.hello();
 import './style.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-let pages = [];
-for(let i = 10; i<20; i++){
-  pages.push(new HtmlWebpackPlugin({
-    filename: i+'.html',
-    template: './src/views/number.njk',
-    templateParameters: { number: i }
-  }));
-}
-
 module.exports = {
 //   mode: 'production',
   entry: './src/index.js',
@@ -35,27 +26,11 @@ module.exports = {
       {
         test: /\.s[ac]ss$/i,
         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.njk$/,
-        use: [
-            {
-                loader: 'simple-nunjucks-loader',
-                options: {}
-            }
-        ]
       }
     ],
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/views/index.njk'
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'about.html',
-      template: './src/views/about.njk'
-    }),
+    new HtmlWebpackPlugin(),
     new MiniCssExtractPlugin(),
-    ...pages,
   ],
 };
