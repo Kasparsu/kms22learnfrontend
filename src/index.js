5d737996a1b08702c1a8a4a012fda69739cdac03
Kaspar Martin Suursalu <kasparsu@gmail.com>
2024-01-27 12:17:29 +0200

    Add Modal example

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router
 
 import Home from './pages/Home.vue';
 import ToDo from './pages/ToDo.vue';
-
+import Modals from './pages/Modals.vue';
 const routes = [
     { path: '/', component: Home, name: 'Home' },
     { path: '/todo', component: ToDo, name: 'ToDo' },
-
+    { path: '/modals', component: Modals, name: 'Modals' },
 ];
 
 const router = createRouter({
---
+++ src/pages/Modals.vue
@@ -0,0 +1,29 @@
+<template>
+    <button class="button is-primary" @click="modalOpen = true">Open Modal</button>
+    <Modal :open="modalOpen" @close="modalOpen = false">
+        <p class="image is-4by3">
+            <img src="https://bulma.io/images/placeholders/1280x960.png" alt="">
+        </p>
+    </Modal>
+
+    <button class="button is-primary" @click="modalOpen2 = true">Open Modal2</button>
+    <Modal :open="modalOpen2" @close="modalOpen2 = false">
+        <p class="image is-4by3">
+            <img src="http://placekitten.com/1280/960" alt="">
+        </p>
+    </Modal>
+
+    <button class="button is-primary" @click="modalOpen3 = true">Open Modal3</button>
+    <Modal :open="modalOpen3" @close="modalOpen3 = false">
+        <p class="image is-4by3">
+            <img src="http://placebear.com/1280/960" alt="">
+        </p>
+    </Modal>
+</template>
+<script setup>
+import { ref } from 'vue';
+import Modal from '../components/Modal.vue';
+let modalOpen = ref(false);
+let modalOpen2 = ref(false);
+let modalOpen3 = ref(false);
+</script>
